a2b7bed1703f4db719f563cace69a2f27819eef0
Giovanni Ramirez <jesussavedgr78@gmail.com>
2024-04-12 00:52:46 -0400

    Got the data from index.html to moviepage.html

---
+++ assets/js/moviepage.js
@@ -0,0 +1,22 @@
+const moviePageContainer = document.querySelector("#movie-page");
+const movieLS = localStorage.getItem('movieId');
+
+
+function fetchMovieID (event) {
+    const movieId = movieLS;
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDA2YjZiMTA4ZTlkYzk4NTRkMzNiZWU3M2JmZTUwMSIsInN1YiI6IjY2MTU5M2FhMTVhNGExMDE0YWY3ZTM5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9GPo1YyE3aDYpIfjnQjt_1vO4ZuQId0pH1v0TYhhdK4'
+        }
+    };
+      
+    fetch(`https://api.themoviedb.org/3/movie/${movieId}?language=en-US`, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
+}
+
+fetchMovieID();
--- assets/js/script.js
+++ assets/js/script.js
@@ -28,12 +28,16 @@ function titleResults(data) {
         console.log("Card is being made");
         const imageLocation = movieArray[i].poster_path;
         const movieTitle = movieArray[i].original_title;
+        const movieId = movieArray[i].id;
         const image = `https://media.themoviedb.org/t/p/w300_and_h450_bestv2` + imageLocation
 
         // This is the CARD
         const card = document.createElement("div");
         card.setAttribute("class", "card");
         card.setAttribute("style", "width: 18rem;");
+        card.setAttribute('data-movie-id', movieId);
+        card.setAttribute('onClick', "sendMovieIdToLocalStorage(this)")
+        // card.addEventListener('click', sendMovieIdToLocalStorage);
 
         const img = document.createElement("img");
         img.setAttribute("src", image);
@@ -85,3 +89,38 @@ searchFormEl.addEventListener('submit', moviesByGenre);
 $(document).ready(function () {
     window.onload = loadMovie();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// Gio was working here last
+const tempMovieCardTestButton = document.querySelector('#movieCardClick');
+// This WILL be the data-movie-id attach to the movie click event
+
+function sendMovieIdToLocalStorage (event) {
+    console.log(event)
+    const movieId = event.dataset.movieId;
+    
+    if (localStorage.getItem('movieId') !== null) {
+        let movieKey = localStorage.getItem('movieId');
+        movieKey = movieId;
+        localStorage.setItem('movieId', movieKey);
+        console.log("This ran 1");
+    } else {
+        let emptyMovieId = 0;
+        emptyMovieId = movieId;
+        localStorage.setItem('movieId', emptyMovieId);
+        console.log("This ran 2");
+    }
+
+    var myWindow = window.open("moviepage.html", "_self");
+    myWindow
+}
